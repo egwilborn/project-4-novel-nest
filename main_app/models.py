530d3900bc6c7dca1a279ec3b3c1d0210577acb3
PleistocenePeater-p <alexhughes42x@gmail.com>
2023-08-21 07:18:13 -0700

    Added Genre model

--- main_app/models.py
+++ main_app/models.py
@@ -2,5 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
+class Genre(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    subscribers = models.ManyToManyField(User)
 
